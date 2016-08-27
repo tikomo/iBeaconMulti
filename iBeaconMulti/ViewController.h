0a4abaac0f62d798ec12dd17043226340c9e07d2
KomoritaTsuyoshi <komoritatsuyoshi@komoritatsuyoshi-no-imac.local>
2016-08-27 20:07:53 +0900

    Initial Commit

---
+++ iBeaconMulti/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iBeaconMulti
+//
+//  Created by KomoritaTsuyoshi on 2016/08/27.
+//  Copyright © 2016年 KomoritaTsuyoshi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ iBeaconMulti/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  iBeaconMulti
+//
+//  Created by KomoritaTsuyoshi on 2016/08/27.
+//  Copyright © 2016年 KomoritaTsuyoshi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
